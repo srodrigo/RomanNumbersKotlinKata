4b1994f5402dd58c36ae7ebb6880eca630a1f3cb
Sergio Rodrigo <srodrigo@MacBook-Pro-de-Sergio.local>
2015-10-04 15:41:05 +0200

    Added tests for the basic numbers.

--- src/me/srodrigo/roman_kata/RomanNumbers.kt
+++ src/me/srodrigo/roman_kata/RomanNumbers.kt
@@ -1,5 +1,23 @@
 package me.srodrigo.roman_kata
 
+const val ONE = 'I'
+const val FIVE = 'V'
+const val TEN = 'X'
+const val FIFTY = 'L'
+const val HUNDRED = 'C'
+const val FIVE_HUNDRED = 'D'
+const val THOUSAND = 'M'
+
+private val conversion = mapOf(
+        ONE to 1,
+        FIVE to 5,
+        TEN to 10,
+        FIFTY to 50,
+        HUNDRED to 100,
+        FIVE_HUNDRED to 500,
+        THOUSAND to 1000
+)
+
 fun romanToArabic(roman: String): Int {
-    return 1
+    return conversion[roman.first()]!!
 }
--- test/me/srodrigo/roman_kata/RomanNumbersTest.kt
+++ test/me/srodrigo/roman_kata/RomanNumbersTest.kt
@@ -6,6 +6,30 @@ import org.junit.Test
 class RomanNumbersTest {
 
     @Test fun testOne() {
-        Assert.assertEquals(1, romanToArabic("I"));
+        Assert.assertEquals(1, romanToArabic(ONE.toString()));
+    }
+
+    @Test fun testFive() {
+        Assert.assertEquals(5, romanToArabic(FIVE.toString()));
+    }
+
+    @Test fun testTen() {
+        Assert.assertEquals(10, romanToArabic(TEN.toString()));
+    }
+
+    @Test fun testFifty() {
+        Assert.assertEquals(50, romanToArabic(FIFTY.toString()));
+    }
+
+    @Test fun testHundred() {
+        Assert.assertEquals(100, romanToArabic(HUNDRED.toString()));
+    }
+
+    @Test fun testFiveHundred() {
+        Assert.assertEquals(500, romanToArabic(FIVE_HUNDRED.toString()));
+    }
+
+    @Test fun testThousand() {
+        Assert.assertEquals(1000, romanToArabic(THOUSAND.toString()));
     }
 }
